7a33b731776a3927ee6543b92e5df2c6fed18fe0
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-10-05 21:10:19 +0600

    character checker

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ console.log(totalFine("65"));
 console.log(totalFine("Gorib tai ticket katinai"));
 
 console.log("------------------------------");
+
+// character checker
+function onlyCharacter(str) {
+  // You have to write your code here
+  if (typeof str === "string") {
+    let newStr = "";
+    str.split(" ").forEach((word) => {
+      newStr += word;
+    });
+    return newStr.toUpperCase();
+  } else {
+    return "Invalid";
+  }
+}
+console.log(onlyCharacter("  h e llo wor   ld"));
+console.log(onlyCharacter("Cy   bar- At  tac k  "));
+console.log(onlyCharacter(" ha ck m e 1 @ru.c  n  "));
+console.log(onlyCharacter("Serv er : : Do wn"));
+console.log(onlyCharacter(["hack", "me"]));
+console.log(onlyCharacter(true));
+
+console.log("------------------------------");
