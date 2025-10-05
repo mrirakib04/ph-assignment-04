28271687e2c5520d1ccfca90efdedcdbd17003f7
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-10-05 20:18:24 +0600

    fine calculation || added

---
+++ script.js
@@ -0,0 +1,19 @@
+// fine calculation
+function totalFine(fare) {
+  // You have to write your code here
+  if (typeof fare === "number" && fare > 0) {
+    // return (fare + fare * (20 / 100) + 30).toFixed(2);
+    return fare + fare * (20 / 100) + 30;
+  } else {
+    return "Invalid";
+  }
+}
+console.log(totalFine(200));
+console.log(totalFine(0));
+console.log(totalFine(50));
+console.log(totalFine(552));
+console.log(totalFine(-35));
+console.log(totalFine("65"));
+console.log(totalFine("Gorib tai ticket katinai"));
+
+console.log("------------------------------");
